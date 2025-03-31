a3197288278fce3895d8f74b3e0900daf4083e54
Nauczyciel120 <Nauczyciel120@DESKTOP-5L8KCDI>
2025-03-31 12:13:12 +0200

    Dodaj pliki projektów.

---
+++ mnozenie/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace mnozenie
+{
+    internal class Program
+    {
+        public int n;
+        Program(int n)
+        {
+            this.n = n;
+        }
+
+       public void losowanie(Int128 [,] a, Int128[,] b, int min, int max) 
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = rnd.Next(min, max);
+                    b[i, j] = rnd.Next(min, max);
+                }
+            }
+        }
+
+        public void wyswietlanie(Int128[,] a)
+        {
+            for (int i = 0; i < this.n; i++)
+            {
+                for (int j = 0; j < this.n; j++)
+                {
+                    Console.Write(a[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("*******************************************");
+        }
+
+        public void mnozenie(Int128[,] a, Int128[,] b, Int128[,] c) {
+
+            for (int i = 0; i < this.n; i++)
+            {
+                for (int j = 0; j < this.n; j++)
+                {
+                    c[i, j] = 0;
+                    for (int k = 0; k < this.n; k++)
+                    {
+                        c[i, j] += a[i, k] * b[k, j];
+                    }
+
+                }
+            }
+
+
+                }
+
+            static void Main(string[] args)
+        {
+          //  int n = 10;
+
+            Program obiekt = new Program(3);
+
+            Int128[,] a = new Int128[obiekt.n, obiekt.n];
+            Int128[,] b = new Int128[obiekt.n, obiekt.n];
+            Int128[,] c = new Int128[obiekt.n, obiekt.n];
+
+            obiekt.losowanie(a,b,1,4);
+            obiekt.wyswietlanie(a);
+            obiekt.wyswietlanie(b);
+            obiekt.mnozenie(a, b, c);
+            obiekt.wyswietlanie(c);
+
+
+        }
+    }
+}
